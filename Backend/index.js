7ac8e21844315fa2aec0331090377ecb3204e39a
Vikash Singh <97822077+VikashSingh0007@users.noreply.github.com>
2023-03-24 12:52:23 +0530

    Adding authentication

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-
+const authRoute = require("./routes/auth")
 dotenv.config()
 
 mongoose
@@ -11,7 +11,7 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err))
-
+app.use("/auth", authRoute)
   app.listen("5000", () => {
     console.log("backend running...")
   })
