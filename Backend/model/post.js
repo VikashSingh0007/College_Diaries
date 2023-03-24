592f47afaa4d25a7d6cfeb049a4bcb410ed43996
adiroy05 <96076675+adiroy05@users.noreply.github.com>
2023-03-24 20:38:37 +0530

    Schema model updated

---
+++ Backend/model/post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    Clgname: {
+      type: String,
+      require: true,
+      
+      },
+    desc: {
+      type: String,
+      require: true,
+    },
+    photo: {
+      type: String,
+      require: false,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    categories: {
+      type: Array,
+      require: false,
+    },
+  },
+  { timestamps: true }
+)
+module.exports = mongoose.model("Post", PostSchema)
