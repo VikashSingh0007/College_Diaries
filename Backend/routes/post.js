5f6bdb8e02c685212c1432d0e2df059c7986ee6d
adiroy05 <96076675+adiroy05@users.noreply.github.com>
2023-04-13 02:41:35 +0530

    Update post

--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,34 +1,93 @@
-const mongoose = require("mongoose")
+const router = require("express").Router()
+const Post = require("../model/Post")
 
-const PostSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      require: true,
-      unique: true,
-    },
-    Clgname: {
-      type: String,
-      require: true,
-      
-      },
-    desc: {
-      type: String,
-      require: true,
-    },
-    photo: {
-      type: String,
-      require: false,
-    },
-    username: {
-      type: String,
-      require: true,
-    },
-    categories: {
-      type: Array,
-      require: false,
-    },
-  },
-  { timestamps: true }
-)
-module.exports = mongoose.model("Post", PostSchema)
+
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body)
+  try {
+    const savePost = await newPost.save()
+    res.status(200).json(savePost)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        const updatePost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        )
+        res.status(200).json(updatePost)
+      } catch (error) {
+        res.status(500).json(error)
+      }
+    } else {
+      res.status(401).json("You can update only your post!")
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        await post.delete()
+        res.status(200).json("Post Has been delete!")
+      } catch (error) {
+        res.status(500).json(error)
+      }
+    } else {
+      res.status(401).json("You can delete only your post!")
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    res.status(200).json(post)
+  } catch (error) {
+    res.status(404).json(error)
+  }
+})
+
+
+router.get("/", async (req, res) => {
+  const username = req.query.user
+  const catName = req.query.cat
+  try {
+    let posts
+    if (username) {
+      posts = await Post.find({ username: username })
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      })
+    } else {
+      posts = await Post.find()
+    }
+    res.status(200).json(posts)
+  } catch (error) {
+    res.status(404).json(error)
+  }
+})
+
+
+
+module.exports = router
