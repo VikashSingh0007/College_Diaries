9d26b55202cfc22b855c09b7e9b90cdb155a4138
adiroy05 <96076675+adiroy05@users.noreply.github.com>
2023-08-14 18:51:19 +0530

    Update Footer.jsx

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,18 +3,38 @@ import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai"
 import { BsFacebook } from "react-icons/bs"
 import { RiInstagramFill } from "react-icons/ri"
 
+const mystyle = 
+{
+  position: "relative",
+  left: "0",
+  bottom: "0",
+  width: "100%"
+};
 
+const mystyle3 = 
+{
+  fontSize: "15px",
+transform:"translateX(20px)"
+};
 export const Footer = () => {
+
+ 
+  
   return (
     <>
-      <footer className='boxItems'>
+      <footer style={mystyle} className='boxItems'>
         <div className='container flex'>
-          <p>MADE WITH lOVE AT & vk & AR</p>
-          <div className='social'>
-           <a href="https://leetcode.com/problemset/all/" target="_blank" rel="noreferrer"><BsFacebook className='icon' /></a>
-            <RiInstagramFill className='icon' />
-            <AiFillTwitterCircle className='icon' />
-            <AiFillLinkedin className='icon' />
+          <p style={mystyle3}>MADE WITH lOVE AR & VS & AT</p>
+          <div className='social'   >
+           <a style={{
+ color:"black"
+}} href="https://www.facebook.com/aditya.roysha/" target="_blank" rel="noreferrer"><BsFacebook className='icon' /></a>
+           <a  style={{
+ color:"black"
+}} href="https://www.instagram.com/singhvikash7077/" target="_blank" rel="noreferrer"><RiInstagramFill className='icon' /></a>
+           <a  style={{
+ color:"black"
+}} href="https://www.linkedin.com/in/arinjaythakur/" target="_blank" rel="noreferrer"><AiFillLinkedin className='icon' /></a>
           </div>
         </div>
       </footer>
